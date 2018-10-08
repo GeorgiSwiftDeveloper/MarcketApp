dc8a3310132741e6cc8b9bc4e7ab6cfac7fe0369
Georgi Malkhasyan <georgimalkhasyan@georgis-macbook-pro.local>
2018-10-08 14:41:09 -0700

    Initial Commit

---
+++ myMarcket/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myMarcket
+//
+//  Created by Georgi Malkhasyan on 10/8/18.
+//  Copyright © 2018 Adamyan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
